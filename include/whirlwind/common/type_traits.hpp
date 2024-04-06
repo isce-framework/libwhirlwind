652780aae17245fb3434542560ce4658e5b4cd59
Geoffrey M Gunter <geoffrey.m.gunter@gmail.com>
2024-04-06 00:04:49 -0700

    Remove ranges type traits

--- include/whirlwind/common/type_traits.hpp
+++ include/whirlwind/common/type_traits.hpp
@@ -11,17 +11,4 @@ WHIRLWIND_NAMESPACE_BEGIN
 template<class T>
 using remove_cvref_t = std::remove_cv_t<std::remove_reference_t<T>>;
 
-namespace ranges {
-
-template<class Range>
-using iterator_type = remove_cvref_t<decltype(std::begin(std::declval<Range>()))>;
-
-template<class Range>
-using size_type = typename Range::size_type;
-
-template<class Range>
-using value_type = remove_cvref_t<decltype(*std::begin(std::declval<Range>()))>;
-
-} // namespace ranges
-
 WHIRLWIND_NAMESPACE_END
