d86fcff6c5a843eb51a40d4bff666d718e5faa56
Geoffrey M Gunter <geoffrey.m.gunter@gmail.com>
2024-06-24 21:26:09 -0700

    Fix residue sign convention; Fix phase gradient integration step

--- include/whirlwind/integrate_unwrapped_gradients.hpp
+++ include/whirlwind/integrate_unwrapped_gradients.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cmath>
+#include <type_traits>
 #include <vector>
 
 #include "common/namespace.hpp"
@@ -13,77 +14,153 @@
 WHIRLWIND_NAMESPACE_BEGIN
 
 template<template<class> class Container = std::vector,
+         class Accumulator = double,
          class ArrayLike2D,
          class Dim,
          class Cost,
          class Flow,
          // clang-format off
-         template<class> class ContainerU,
+         template<class> class UContainer,
          // clang-format on
          class Mixin>
 [[nodiscard]] constexpr auto
 integrate_unwrapped_gradients(
         const ArrayLike2D& wrapped_phase,
-        const Network<RectangularGridGraph<1, Dim>, Cost, Flow, ContainerU, Mixin>&
+        const Network<RectangularGridGraph<1, Dim>, Cost, Flow, UContainer, Mixin>&
                 network)
 {
+    // The input wrapped phase array must be a real-valued 2-D array.
+    using Real = typename ArrayLike2D::value_type;
+    WHIRLWIND_STATIC_ASSERT(std::is_floating_point_v<Real>);
     using Extents = typename ArrayLike2D::extents_type;
     WHIRLWIND_STATIC_ASSERT(Extents::rank() == 2);
 
+    // `Flow` must be a signed type. The sign of the net flow between two nodes is used
+    // to indicate the direction of flow.
+    WHIRLWIND_STATIC_ASSERT(std::is_signed_v<Flow>);
+
+    // Check that the wrapped phase array and network grid graph have compatible shapes.
+    // The graph is expected to contain exactly one more row and one more column of
+    // nodes than the array dimensions.
     const auto m = wrapped_phase.extent(0);
     const auto n = wrapped_phase.extent(1);
-    WHIRLWIND_ASSERT(m >= 1);
-    WHIRLWIND_ASSERT(n >= 1);
-
     const auto& residual_graph = network.residual_graph();
     WHIRLWIND_ASSERT(residual_graph.num_rows() == m + 1);
     WHIRLWIND_ASSERT(residual_graph.num_cols() == n + 1);
 
-    using Real = remove_cvref_t<typename ArrayLike2D::value_type>;
+    // TODO: Check that the flow in the network is feasible
+
+    // Initialize the output array.
     auto unwrapped_phase = Array2D<Real, Container<Real>>(m, n);
 
+    // If the input array is Mx0 or 0xN, there's nothing to do.
+    if ((m == 0) || (n == 0)) {
+        return unwrapped_phase;
+    }
+
+    // Checks whether the argument is in the interval [-pi, pi].
+    [[maybe_unused]] auto is_wrapped_phase = [](const auto& psi) {
+        using T = decltype(psi);
+        return (psi >= -pi_v<T>) && (psi <= pi_v<T>);
+    };
+
+    // Computes the difference between the two input phase values (in radians), wrapped
+    // to the interval [-pi, pi).
     auto wrapped_diff = [](const auto& a, const auto& b) {
-        const auto diff = b - a;
-        return diff - tau_v<decltype(diff)> * std::round(diff / tau_v<decltype(diff)>);
+        const auto diff = a - b;
+        using T = decltype(diff);
+        return diff - tau_v<T> * std::round(diff / tau_v<T>);
     };
 
     using ResidualGraph = remove_cvref_t<decltype(residual_graph)>;
     using Vertex = ResidualGraph::vertex_type;
 
+    // Start with a fixed "seed" point where the wrapped and unwrapped phase values are
+    // forced to be equal.
     unwrapped_phase(0, 0) = wrapped_phase(0, 0);
 
+    // Scan down the first column. Accumulate the unwrapped phase gradients between each
+    // adjacent pair of pixels to get the unwrapped phase values.
+    auto phi = Accumulator{unwrapped_phase(0, 0)};
     using Index = std::remove_const_t<decltype(m)>;
     for (Index i = 1; i < m; ++i) {
-        const auto phi0 = unwrapped_phase(i - 1, 0);
-
+        // Compute the wrapped phase gradient between the pair of adjacent phase values.
         const auto psi0 = wrapped_phase(i - 1, 0);
         const auto psi1 = wrapped_phase(i, 0);
-        const auto dpsi = wrapped_diff(psi0, psi1);
+        WHIRLWIND_ASSERT(is_wrapped_phase(psi0));
+        WHIRLWIND_ASSERT(is_wrapped_phase(psi1));
+        const auto dpsi = wrapped_diff(psi1, psi0);
+        WHIRLWIND_DEBUG_ASSERT(is_wrapped_phase(dpsi));
 
-        const auto node = Vertex(i, 0);
-        WHIRLWIND_DEBUG_ASSERT(residual_graph.contains_vertex(node));
-        const auto arc = residual_graph.get_down_edge(node);
-        WHIRLWIND_DEBUG_ASSERT(residual_graph.contains_edge(arc));
-        const auto x = network.arc_flow(arc);
+        // Get the two residues (nodes) in the network that both border the edge between
+        // the pair of pixels.
+        WHIRLWIND_DEBUG_ASSERT(i < residual_graph.num_rows());
+        WHIRLWIND_DEBUG_ASSERT(1 < residual_graph.num_cols());
+        const auto node0 = Vertex(i, 0);
+        const auto node1 = Vertex(i, 1);
+        WHIRLWIND_DEBUG_ASSERT(residual_graph.contains_vertex(node0));
+        WHIRLWIND_DEBUG_ASSERT(residual_graph.contains_vertex(node1));
 
-        unwrapped_phase(i, 0) = phi0 + dpsi + tau_v<decltype(dpsi)> * x;
+        // Get the net leftward flow between the two neighboring residues. If the
+        // residues were formed from clockwise loops, this corresponds to the difference
+        // (in cycles) between the unwrapped & wrapped phase gradients in the downward
+        // direction (from the upper to the lower pixel).
+        const auto arc0 = residual_graph.get_right_edge(node0);
+        const auto arc1 = residual_graph.get_left_edge(node1);
+        WHIRLWIND_DEBUG_ASSERT(residual_graph.contains_edge(arc0));
+        WHIRLWIND_DEBUG_ASSERT(residual_graph.contains_edge(arc1));
+        const auto net_flow = network.arc_flow(arc1) - network.arc_flow(arc0);
+
+        // Get the unwrapped phase gradient between the pixels and add it to the
+        // cumulative sum.
+        const auto dphi = dpsi + tau_v<decltype(dpsi)> * net_flow;
+        phi += Accumulator{dphi};
+
+        // Store the unwrapped phase value.
+        unwrapped_phase(i, 0) = static_cast<Real>(phi);
     }
 
+    // Scan across each row. Accumulate the unwrapped phase gradients between each
+    // adjacent pair of pixels to get the unwrapped phase values.
     for (Index i = 0; i < m; ++i) {
-        for (Index j = 1; j < n; ++j) {
-            const auto phi0 = unwrapped_phase(i, j - 1);
+        auto phi = Accumulator{unwrapped_phase(i, 0)};
 
+        for (Index j = 1; j < n; ++j) {
+            // Compute the wrapped phase gradient between the pair of adjacent phase
+            // values.
             const auto psi0 = wrapped_phase(i, j - 1);
             const auto psi1 = wrapped_phase(i, j);
-            const auto dpsi = wrapped_diff(psi0, psi1);
+            WHIRLWIND_ASSERT(is_wrapped_phase(psi0));
+            WHIRLWIND_ASSERT(is_wrapped_phase(psi1));
+            const auto dpsi = wrapped_diff(psi1, psi0);
+            WHIRLWIND_DEBUG_ASSERT(is_wrapped_phase(dpsi));
 
-            const auto node = Vertex(i, j);
-            WHIRLWIND_DEBUG_ASSERT(residual_graph.contains_vertex(node));
-            const auto arc = residual_graph.get_right_edge(node);
-            WHIRLWIND_DEBUG_ASSERT(residual_graph.contains_edge(arc));
-            const auto x = network.arc_flow(arc);
+            // Get the two residues (nodes) in the network that both border the edge
+            // between the pair of pixels.
+            WHIRLWIND_DEBUG_ASSERT(i + 1 < residual_graph.num_rows());
+            WHIRLWIND_DEBUG_ASSERT(j < residual_graph.num_cols());
+            const auto node0 = Vertex(i, j);
+            const auto node1 = Vertex(i + 1, j);
+            WHIRLWIND_DEBUG_ASSERT(residual_graph.contains_vertex(node0));
+            WHIRLWIND_DEBUG_ASSERT(residual_graph.contains_vertex(node1));
 
-            unwrapped_phase(i, j) = phi0 + dpsi + tau_v<decltype(dpsi)> * x;
+            // Get the net downward flow between the two neighboring residues. If the
+            // residues were formed from clockwise loops, this corresponds to the
+            // difference (in cycles) between the unwrapped & wrapped phase gradients in
+            // the rightward direction (from the left to the right pixel).
+            const auto arc0 = residual_graph.get_down_edge(node0);
+            const auto arc1 = residual_graph.get_up_edge(node1);
+            WHIRLWIND_DEBUG_ASSERT(residual_graph.contains_edge(arc0));
+            WHIRLWIND_DEBUG_ASSERT(residual_graph.contains_edge(arc1));
+            const auto net_flow = network.arc_flow(arc0) - network.arc_flow(arc1);
+
+            // Get the unwrapped phase gradient between the pixels and add it to the
+            // cumulative sum.
+            const auto dphi = dpsi + tau_v<decltype(dpsi)> * net_flow;
+            phi += Accumulator{dphi};
+
+            // Store the unwrapped phase value.
+            unwrapped_phase(i, j) = static_cast<Real>(phi);
         }
     }
 
--- include/whirlwind/residue.hpp
+++ include/whirlwind/residue.hpp
@@ -30,8 +30,14 @@ residue(const ArrayLike2D& wrapped_phase)
     WHIRLWIND_ASSERT(n >= 1);
     auto out = Array2D<SignedInteger, Container<SignedInteger>>(m + 1, n + 1);
 
+    // Checks whether the argument is in the interval [-pi, pi].
+    [[maybe_unused]] auto is_wrapped_phase = [](const auto& psi) {
+        using T = decltype(psi);
+        return (psi >= -pi_v<T>) && (psi <= pi_v<T>);
+    };
+
     auto cycle_diff_residual = [](const auto& a, const auto& b) -> SignedInteger {
-        const auto diff = b - a;
+        const auto diff = a - b;
         return static_cast<SignedInteger>(std::round(diff / tau_v<decltype(diff)>));
     };
 
@@ -41,23 +47,20 @@ residue(const ArrayLike2D& wrapped_phase)
             const auto phi_00 = wrapped_phase(i, j);
             const auto phi_10 = wrapped_phase(i + 1, j);
             const auto phi_01 = wrapped_phase(i, j + 1);
-            WHIRLWIND_ASSERT(phi_00 >= -pi_v<decltype(phi_00)> &&
-                             phi_00 <= pi_v<decltype(phi_00)>);
-            WHIRLWIND_ASSERT(phi_10 >= -pi_v<decltype(phi_10)> &&
-                             phi_10 <= pi_v<decltype(phi_10)>);
-            WHIRLWIND_ASSERT(phi_01 >= -pi_v<decltype(phi_01)> &&
-                             phi_01 <= pi_v<decltype(phi_01)>);
+            WHIRLWIND_ASSERT(is_wrapped_phase(phi_00));
+            WHIRLWIND_ASSERT(is_wrapped_phase(phi_10));
+            WHIRLWIND_ASSERT(is_wrapped_phase(phi_01));
 
-            const auto di = cycle_diff_residual(phi_10, phi_00);
-            const auto dj = cycle_diff_residual(phi_00, phi_01);
+            const auto di = cycle_diff_residual(phi_00, phi_10);
+            const auto dj = cycle_diff_residual(phi_01, phi_00);
             WHIRLWIND_DEBUG_ASSERT((di == -1) || (di == 0) || (di == 1));
             WHIRLWIND_DEBUG_ASSERT((dj == -1) || (dj == 0) || (dj == 1));
 
             WHIRLWIND_DEBUG_ASSERT(i + 1 < out.extent(0));
             WHIRLWIND_DEBUG_ASSERT(j + 1 < out.extent(1));
-            out(i + 1, j) -= di;
-            out(i, j + 1) -= dj;
-            out(i + 1, j + 1) += di + dj;
+            out(i + 1, j) += di;
+            out(i, j + 1) += dj;
+            out(i + 1, j + 1) -= di + dj;
         }
     }
 
@@ -65,32 +68,32 @@ residue(const ArrayLike2D& wrapped_phase)
     for (Index i = 0, j = n - 1; i < m - 1; ++i) {
         const auto phi0 = wrapped_phase(i, j);
         const auto phi1 = wrapped_phase(i + 1, j);
-        WHIRLWIND_ASSERT(phi0 >= -pi_v<decltype(phi0)> && phi0 <= pi_v<decltype(phi0)>);
-        WHIRLWIND_ASSERT(phi1 >= -pi_v<decltype(phi1)> && phi1 <= pi_v<decltype(phi1)>);
-
-        const auto d = cycle_diff_residual(phi1, phi0);
-        WHIRLWIND_DEBUG_ASSERT((d == -1) || (d == 0) || (d == 1));
-
-        WHIRLWIND_DEBUG_ASSERT(i + 1 < out.extent(0));
-        WHIRLWIND_DEBUG_ASSERT(j + 1 < out.extent(1));
-        out(i + 1, j) -= d;
-        out(i + 1, j + 1) += d;
-    }
-
-    // Last row.
-    for (Index i = m - 1, j = 0; j < n - 1; ++j) {
-        const auto phi0 = wrapped_phase(i, j);
-        const auto phi1 = wrapped_phase(i, j + 1);
-        WHIRLWIND_ASSERT(phi0 >= -pi_v<decltype(phi0)> && phi0 <= pi_v<decltype(phi0)>);
-        WHIRLWIND_ASSERT(phi1 >= -pi_v<decltype(phi1)> && phi1 <= pi_v<decltype(phi1)>);
+        WHIRLWIND_ASSERT(is_wrapped_phase(phi0));
+        WHIRLWIND_ASSERT(is_wrapped_phase(phi1));
 
         const auto d = cycle_diff_residual(phi0, phi1);
         WHIRLWIND_DEBUG_ASSERT((d == -1) || (d == 0) || (d == 1));
 
         WHIRLWIND_DEBUG_ASSERT(i + 1 < out.extent(0));
         WHIRLWIND_DEBUG_ASSERT(j + 1 < out.extent(1));
-        out(i, j + 1) -= d;
-        out(i + 1, j + 1) += d;
+        out(i + 1, j) += d;
+        out(i + 1, j + 1) -= d;
+    }
+
+    // Last row.
+    for (Index i = m - 1, j = 0; j < n - 1; ++j) {
+        const auto phi0 = wrapped_phase(i, j);
+        const auto phi1 = wrapped_phase(i, j + 1);
+        WHIRLWIND_ASSERT(is_wrapped_phase(phi0));
+        WHIRLWIND_ASSERT(is_wrapped_phase(phi1));
+
+        const auto d = cycle_diff_residual(phi1, phi0);
+        WHIRLWIND_DEBUG_ASSERT((d == -1) || (d == 0) || (d == 1));
+
+        WHIRLWIND_DEBUG_ASSERT(i + 1 < out.extent(0));
+        WHIRLWIND_DEBUG_ASSERT(j + 1 < out.extent(1));
+        out(i, j + 1) += d;
+        out(i + 1, j + 1) -= d;
     }
 
     return out;
